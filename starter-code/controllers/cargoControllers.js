d4ae2e6537ff7be3cff16c6883f7177a7bfef508
Mazkid21 <zmaraziti@yahoo.com>
2017-07-31 12:12:32 -0600

    files working in MVC format

---
+++ starter-code/controllers/cargoControllers.js
@@ -0,0 +1,27 @@
+var db = require('../models');
+
+
+var cargoShow = function(req, res) {
+	db.Cargo.find({}, function(error, cargo){
+		res.render('cargoShowAll', {cargo: cargo});
+	});
+	
+};
+
+//Cargo form
+ var cargoGet = function(req, res) { //look at that controller
+	res.render('cargoNew'); 
+};
+
+//Add new cargo
+var cargoPost = function(req, res) { //and look at that controller
+	db.Cargo.create({description: req.body.description, title: req.body.title}, function(error, cargo) {
+		res.render('cargoShow', {cargo: cargo});
+	});
+};
+
+module.exports.cargoGet = cargoGet;
+
+module.exports.cargoPost = cargoPost;
+
+module.exports.cargoShow = cargoShow;
